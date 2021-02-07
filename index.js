d92f6e17cea6475b39eb51d1af7f4d4e131add42
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-07 07:19:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ let randomNumber = Math.floor(Math.random() * 21)
 
 let randomInteger = Math.floor(randomNumber)
 
-let randomUserID = Math.random(1000000001, 1000000020)
+let randomUserID = Math.floor(Math.random() * 
